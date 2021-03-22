b17bfb7c19aa3ee56fc030fd161eedce1a32394c
Łukasz Walczak <43341428+LukaszBielsko@users.noreply.github.com>
2021-03-22 08:33:43 +0100

    minor changes

--- src/utils/NormalizeData.js
+++ src/utils/NormalizeData.js
@@ -10,22 +10,3 @@ export const articleEntity = new schema.Entity('articles', {
   author: userEntity,
   comments: [commentEntity]
 });
-
-const originalData = {
-  "id": "4532",
-  "author": {
-    "id": "14543",
-    "name": "Lukasz"
-  },
-  "title": "This is a title!",
-  "comments": [
-    {
-      "id": "324",
-      "commenter": {
-        "id": "666",
-        "name": "Maciek"
-      }
-    }
-  ]
-}
-
